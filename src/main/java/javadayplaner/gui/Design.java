77d8291a832d30451f50173312fe8c755a3ed1e4
JustKilli <justkilli@outlook.de>
2023-08-04 23:34:28 +0200

    JDP-7 Design class added

---
+++ src/main/java/javadayplaner/gui/Design.java
@@ -0,0 +1,65 @@
+package javadayplaner.gui;
+
+import javax.swing.border.Border;
+import java.awt.*;
+
+public class Design {
+
+    private final Color backgroundColor;
+    private final Color backgroundColorComponents;
+    private final Color textColor;
+    private final Color textColorHeader;
+    private final Font textFont;
+    private final Font textFontHeader;
+    private final Border borderButtons;
+    private final Border borderComponents;
+    private final Border borderHeader;
+
+    public Design(Color backgroundColor, Color backgroundColorComponents, Color textColor, Color textColorHeader, Font textFont, Font textFontHeader, Border borderButtons, Border borderComponents, Border borderHeader) {
+        this.backgroundColor = backgroundColor;
+        this.backgroundColorComponents = backgroundColorComponents;
+        this.textColor = textColor;
+        this.textColorHeader = textColorHeader;
+        this.textFont = textFont;
+        this.textFontHeader = textFontHeader;
+        this.borderButtons = borderButtons;
+        this.borderComponents = borderComponents;
+        this.borderHeader = borderHeader;
+    }
+
+    public Color getBackgroundColor() {
+        return backgroundColor;
+    }
+
+    public Color getBackgroundColorComponents() {
+        return backgroundColorComponents;
+    }
+
+    public Color getTextColor() {
+        return textColor;
+    }
+
+    public Color getTextColorHeader() {
+        return textColorHeader;
+    }
+
+    public Font getTextFont() {
+        return textFont;
+    }
+
+    public Font getTextFontHeader() {
+        return textFontHeader;
+    }
+
+    public Border getBorderButtons() {
+        return borderButtons;
+    }
+
+    public Border getBorderComponents() {
+        return borderComponents;
+    }
+
+    public Border getBorderHeader() {
+        return borderHeader;
+    }
+}
